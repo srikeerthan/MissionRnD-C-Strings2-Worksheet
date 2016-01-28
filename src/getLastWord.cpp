afa5197f140949544bca140a27961bbae0812ebe
srikeerthan <srikeerthan@missionrnd.com>
2016-01-28 21:38:35 +0530

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -11,6 +11,45 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 #include <stdlib.h>
 
 char * get_last_word(char * str){
-
-	return NULL;
+	if (str==NULL)
+		return NULL;
+	else if (str[0] == '\0')
+	{
+		char *word = (char *)malloc(1);
+		word[0] = '\0';
+		return word;
+	}
+	else
+	{
+		int index,first=0,last=0,i,count=0;
+		for (index = 0; str[index]; index++)
+		{
+			if (str[index] == ' ')
+			{
+				if (count == 1)
+				{
+					last = index;
+					count = 0;
+				}
+			}
+			else
+			{
+				if (count == 0)
+				{
+					first = index;
+					count = 1;
+				}
+			}
+		}
+		if (count == 1)
+			last = index;
+		char *last_word = (char *)malloc(last - first+2);
+		for ( i = 0; first< last; first++)
+		{
+			last_word[i] = str[first];
+			i++;
+		}
+		last_word[i] = '\0';
+		return last_word;
+	}
 }
